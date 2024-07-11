7f215759bc8bb6ee099c2c3cd09cac1bf8c70553
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-07-11 19:53:58 +0300

    add initial neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.number = true
+vim.opt.mouse = ''
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣'}
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
