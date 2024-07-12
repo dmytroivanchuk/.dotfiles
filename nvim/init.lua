76eeed4d992eda8c1fa8afeaacf22ca13a58f3f8
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-07-12 13:52:06 +0300

    add lazy.nvim and nvim-treesitter plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,23 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.number = true
-vim.opt.mouse = ''
+vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
 vim.opt.undofile = true
+vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣'}
-vim.opt.inccommand = 'split'
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣"}
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
   callback = function()
     vim.highlight.on_yank()
   end,
 })
+
+require("config.lazy")
