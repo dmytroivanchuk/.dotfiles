0f1db01618f1b1af438a0173800643edfb60b479
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-07-15 18:18:30 +0300

    change minor options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
 vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,5 +18,6 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
+  install = { colorscheme = { "default" } },
   checker = { enabled = true },
 })
