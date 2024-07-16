28a5357c9dc0f8719eab80fd9727fd453889c536
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-07-16 13:04:19 +0300

    add gitsigns plugin

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,27 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" }
+        },
+        signs_unstaged = {
+            add = { text = "+" },
+            change = { text = "~" }
+        },
+        on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+    map('v', '<leader>hu', function() gitsigns.undo_stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<leader>td', gitsigns.toggle_deleted)
+end
+    }
+}
